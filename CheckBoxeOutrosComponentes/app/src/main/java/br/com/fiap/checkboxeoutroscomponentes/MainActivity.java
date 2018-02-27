984a1741499dc58a4deb93a8d74548d7a940ee33
logonrm <logonrm@fiap.com.br>
2018-02-27 20:59:08 -0300

    Add projeto contador

--- CheckBoxeOutrosComponentes/app/src/main/java/br/com/fiap/checkboxeoutroscomponentes/MainActivity.java
+++ CheckBoxeOutrosComponentes/app/src/main/java/br/com/fiap/checkboxeoutroscomponentes/MainActivity.java
@@ -25,5 +25,24 @@ public class MainActivity extends AppCompatActivity {
                 this,
                 "Estou aprendendo Android: " + chkEstouAprendendo.isChecked(),
                 Toast.LENGTH_SHORT).show();
+
+        int selecionado = rdgDificuldade.getCheckedRadioButtonId();
+        String msg = "";
+        switch (selecionado){
+            case R.id.rdbFacil:
+                msg = getString(R.string.f_cil);
+                break;
+            case R.id.rdbMedio:
+                msg = getString(R.string.m_dio);
+                break;
+            case R.id.rdbDificl:
+                msg = getString(R.string.dif_cil);
+                break;
+            default:
+                msg = getString(R.string.nadaSelecionado);
+                break;
+        }
+
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
     }
 }
